000ad899a54216ec7d9d6b2eb61c56cf4d7bf84a
Mike Lee <supahotboi69@gmail.com>
2022-06-09 08:46:26 -0400

    front end work

---
+++ onlyfans-frontend/src/components/Posts.js
@@ -0,0 +1,15 @@
+const posts = [
+    {
+        id: 1,
+        title: 'my post',
+        description: 'this is my post',
+    }
+]
+
+const Posts = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
