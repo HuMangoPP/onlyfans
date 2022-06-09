64d3f4e6079f14b51ce66646215696dcc4364151
Mike Lee <supahotboi69@gmail.com>
2022-06-08 22:35:42 -0400

    added front end

--- routes/comments.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { Model } = require('mongoose');
-const router = express.Router();
-const Comment = require('../models/Comment');
-
-/** GET */
-// comments/:id
-router.get()
